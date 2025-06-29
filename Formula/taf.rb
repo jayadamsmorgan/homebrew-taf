13555e84937aee4348514b78d54f0446b34d5ef4
Herman Berdnikov <applejay1802@gmail.com>
2025-06-29 13:08:36 -0400

    TAF v1.0.2

--- Formula/taf.rb
+++ Formula/taf.rb
@@ -1,8 +1,8 @@
 class Taf < Formula
   desc "Testing & Automation Framework in Lua"
   homepage "https://github.com/jayadamsmorgan/taf"
-  url "https://github.com/jayadamsmorgan/taf/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "065e682c56f0370262dd945b2d4d1671be2643593a526e3e3d9a5436bf31a380"
+  url "https://github.com/jayadamsmorgan/taf/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "ebd73858dacceba572b1eb137e6f3a609cf7151467c8abb94e5e5b8e15760cef"
   license "Apache-2.0"
 
   depends_on "meson"      => :build
@@ -20,7 +20,7 @@ class Taf < Formula
   depends_on "notcurses"
 
   def install
-    system "meson", "setup", "build", *std_meson_args, "-Dlua_install_dir=#{pkgshare}/lua"
+    system "meson", "setup", "build", *std_meson_args, "-Dlua_install_dir=#{pkgshare}"
     system "meson", "install", "-C", "build"
   end
 
